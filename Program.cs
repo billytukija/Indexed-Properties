78ffd8db9819b2e2825b6e744e596bf047d24b58
Billy Tukija <billytukij@gmail.com>
2019-11-10 21:27:01 -0300

    Using indexed properties in C#

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndexedProprieties
+{
+    public class Product {
+        
+        List<string> names;
+
+        public Product()
+        {
+            names = new List<string>();
+        }
+
+        public void Add(string name)
+        {
+            names.Add(name);
+        }
+
+        //public string Get(int idx)
+        //{
+        //    return names[idx];
+        //}
+
+        // Indexed proprierties
+
+        public string this [int index]
+        {
+            get {
+                return this.names[index];   
+            }
+        }
+
+        public int this [string name]
+        {
+            get
+            {
+                return this.names.IndexOf(name);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prods = new Product();
+            prods.Add("Billy");
+
+            Console.WriteLine(prods[0]);
+            Console.WriteLine("Billy's position is : " + prods["Billy"]);
+        }
+    }
+}
